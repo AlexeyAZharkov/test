c014079de5913baa825145cbced5abd22f70136d
AlexeyAZharkov <azharkov@mail.ru>
2022-07-01 08:14:09 +0300

    01-07

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        short a;
+        Scanner in = new Scanner(System.in);
+        String w = in.next();
+        String e = in.next();
+
+        short a = 22;
         short d = 10;
-        
-        System.out.println("Hello world!");
+        char v = 's';
+        int r = a + d;
+        System.out.println(r + " - " + e);
+        System.out.print("Hello world!");
+
+        in.close();
     }
 }
